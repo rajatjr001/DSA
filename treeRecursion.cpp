5da904a54530d93f50f151f6ce2b68a70f4e908c
rajat <rajat.jr0001@gmail.com>
2024-09-14 19:22:10 +0530

    .

--- treeRecursion.cpp
+++ treeRecursion.cpp
@@ -1,29 +1,18 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main() {
+void fun(int n){
+    if(n>0){
+        cout<<n<<" ";
+        fun(n-1);
+        fun(n-1);
+    }
+}
 
-  int n1;
+int main(){
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    fun(n);
 
-  printf("Enter percentge : ");
-  scanf("%d",&n1);
-
-
-  if (n1>=90 && n1<100){
-    printf("grade a");
-  }
-  
-  else if( n1>=80 && n1<=89){
-    printf("grade b");
-  }
-
-
-  else if(n1>=70 && n1<=79){
-    printf("grade c");
-  }
-  else if(n1>= 60 && n1<=69){
-    printf("grade d");
-  }
-
-
-  return 0;
 }
